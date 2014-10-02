a8d32239935e317e53f1a41cb962ef4462c9bdfa
roxandquenneville <roxandquenneville@gmail.com>
2014-10-02 14:58:09 -0400

    Revert "Revert "Solution 8""
    This reverts commit d0797289ef88247233cbdd5768b706374e628c5c.

---
+++ La crypte de la créature/La crypte de la créature/Vue/UCPlateau.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace La_crypte_de_la_créature.Vue
+{
+    /// <summary>
+    /// Logique d'interaction pour UCPlateau.xaml
+    /// </summary>
+    public partial class UCPlateau : UserControl
+    {
+        public UCPlateau()
+        {
+            InitializeComponent();
+        }
+    }
+}
