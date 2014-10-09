78487be07dac332270f59ee01a1e752d4529b729
mathieuroy17 <king_math_13@hotmail.com>
2014-10-09 11:28:18 -0400

    remise pointage

--- La crypte de la créature/La crypte de la créature/La crypte de la creature.Logic/Services/NHibernate/mappings/PointageMap.cs
+++ La crypte de la créature/La crypte de la créature/La crypte de la creature.Logic/Services/NHibernate/mappings/PointageMap.cs
@@ -12,24 +12,30 @@ namespace La_crypte_de_la_creature.Logic.Services.NHibernate.Mappings
     {
         public PointageMap()
         {
-              Table("pointages");
-              LazyLoad();
-              Id(x => x.idPointage)
-                .Column("idPointage")
-                .CustomType("Int32")
-                .Access.Property()
-                .CustomSqlType("INTEGER")
-                .Not.Nullable()                
-                .GeneratedBy.Identity();
-               Map(x => x.ListeDePoint)
-                .Column("ipoint")
-                .CustomType<int>()
-                .Access.Property()
-				.Generated.Never()
-                .CustomSqlType("INTEGER");
-			  
-				
-				
+            Table("pointages");
+            LazyLoad();
+            Id(x => x.idPointage)
+              .Column("idPointage")
+              .CustomType("Int32")
+              .Access.Property()
+              .CustomSqlType("INTEGER")
+              .Not.Nullable()
+              .GeneratedBy.Identity();
+            References(x => x.idPartie)
+              .Class<Partie>()
+              .Access.Property()
+              .LazyLoad(Laziness.False)
+              .Cascade.None()
+              .Columns("idPartie");
+            Map(x => x.Point)
+             .Column("point")
+             .CustomType<int>()
+             .Access.Property()
+             .Generated.Never()
+             .CustomSqlType("INTEGER");
+
+
+
         }
     }
 
