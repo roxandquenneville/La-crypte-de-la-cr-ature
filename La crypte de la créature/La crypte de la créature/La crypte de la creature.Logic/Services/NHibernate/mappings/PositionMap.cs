d156eec6ad48480deb1e66b0c1659881cb421a5d
bigpari <mathieu_pariseau@hotmail.com>
2014-10-14 14:36:38 -0400

    ajout de position
    Je crois que cela doit ressemble a ca

---
+++ La crypte de la créature/La crypte de la créature/La crypte de la creature.Logic/Services/NHibernate/mappings/PositionMap.cs
@@ -0,0 +1,32 @@
+﻿using FluentNHibernate.Mapping;
+using La_crypte_de_la_creature.Logic.Modele.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace La_crypte_de_la_creature.Logic.Services.NHibernate.mappings
+{
+    public class PositionMap : ClassMap<Position>
+    {
+        public PositionMap()
+        {
+            Table("position");
+            LazyLoad();
+            Id(x => x.idPosition)
+              .Column("idPosition")
+              .CustomType("Int32")
+              .Access.Property()
+              .CustomSqlType("INTEGER")
+              .Not.Nullable()
+              .GeneratedBy.Identity();
+            References(x => x.idDeplacement)
+               .Class<Deplacement>()
+               .Access.Property()
+               .LazyLoad(Laziness.False)
+               .Cascade.None()
+               .Columns("idDeplacement");
+        }  
+    }
+}
