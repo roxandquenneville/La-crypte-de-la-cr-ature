a7099b7a914454062e0500e834ef34d6e108806a
mathieuroy17 <king_math_13@hotmail.com>
2014-10-21 11:30:34 -0400

    ajout au mapping

---
+++ La crypte de la créature/La crypte de la créature/La crypte de la creature.Logic/Services/NHibernate/mappings/carteMonstreMap.cs
@@ -0,0 +1,47 @@
+﻿using FluentNHibernate.Mapping;
+using La_crypte_de_la_creature.Logic.Modele.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace La_crypte_de_la_creature.Logic.Services.NHibernate.Mappings
+{
+    public class CarteMonstreMap : ClassMap<CartesMonstre>
+    {
+        public CarteMonstreMap()
+        {
+            Table("CartesMonstre");
+            LazyLoad();
+            Id(x => x.idCase)
+              .Column("idCarteMonstre")
+              .CustomType("Int32")
+              .Access.Property()
+              .CustomSqlType("INTEGER")
+              .Not.Nullable()
+              .GeneratedBy.Identity();
+            References(x => x.idPartie)
+            .Class<Plateau>()
+            .Access.Property()
+            .LazyLoad(Laziness.False)
+            .Cascade.None()
+            .Columns("idPartie");
+            Map(x => x.utilise)
+                 .Column("utilise")
+                 .CustomType<bool>()
+                 .Access.Property()
+                 .Generated.Never()
+                 .CustomSqlType("BOOL");
+            Map(x => x.ValeurDeplacement)
+            .Column("valeurDeplacement")
+            .CustomType<int>()
+            .Access.Property()
+            .Generated.Never()
+            .CustomSqlType("INTEGER");
+
+
+        }
+    }
+
+}
