b2818d9516464d01bfacc7d9604fd589104c6c25
mathieuroy17 <king_math_13@hotmail.com>
2014-10-31 15:48:39 -0400

    hello

--- La crypte de la créature/La crypte de la créature/La crypte de la creature.Logic/Modele/Classes/Historique.cs
+++ La crypte de la créature/La crypte de la créature/La crypte de la creature.Logic/Modele/Classes/Historique.cs
@@ -45,5 +45,30 @@ namespace La_crypte_de_la_creature.Logic.Modele.Classes
         {
             return base.GetHashCode();
         }
+
+
+        public virtual string  dernier_Mouvement()
+        {
+            
+           int index = Deplacement.Count();
+           string builder = "Depart : "+Deplacement[index].Depart.X.ToString()+ " , "+Deplacement[index].Depart.Y.ToString() + " / fin:"  +Deplacement[index].Fin.X.ToString()+Deplacement[index].Fin.Y.ToString();
+           return builder;
+        }
+
+        public virtual List<string> liste_Dernier_Mouvement()
+        {
+               Deplacement.Reverse();
+               List<string> lDeplacement=new List<string>();
+            foreach(Deplacement item in Deplacement)
+            {
+               string builder = "Depart : " + item.Depart.X.ToString() + " , " + item.Depart.Y.ToString() + " / fin:" + item.Fin.X.ToString() + item.Fin.Y.ToString();
+               lDeplacement.Add(builder);
+            }
+            Deplacement.Reverse();
+            return lDeplacement;
+            
+        }
+
+        
     }
 }
